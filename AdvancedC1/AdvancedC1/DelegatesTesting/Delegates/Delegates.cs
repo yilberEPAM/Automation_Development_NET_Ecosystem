6c8bb34bfb098c3412d8edbe3ca90fe15bb9a8f0
Yilber Calderon Gonzalez <yilber.calderongonzalez@ukg.com>
2025-10-19 13:24:02 -0500

    removes obsolect project

--- AdvancedC1/AdvancedC1/DelegatesTesting/Delegates/Delegates.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DelegatesTesting.Delegates
-{
-    public class Delegates
-    {
-        public delegate float CalculateTotal(float subtotal);
-        public delegate void CalculateTotalRef(ref float subtotal);
-        public delegate void PrintMessage(string msg);
-    }
-}
